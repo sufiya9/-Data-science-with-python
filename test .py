0a1c20e11da8fd8058905d6e6884fdc93b04e282
sufiya <ansarisufiya507@gmail.com>
2022-09-23 18:33:00 +0530

    webcam and hand detection

---
+++ test .py
@@ -0,0 +1,100 @@
+# # set1={2,5,3}
+# # set2={3,1}
+# # set3={}
+# # set3=set1&set2
+# # print(set3)
+
+# # s='foobar'
+# # print(s[::-1][::-5])
+# # print(s[::-5])
+# # print(s[::5])
+# # print(s[0]+s[-1])
+# # print(s[::-1][-1]+s[len(s)-1])
+
+# # list1=[8,0,9,5]
+# # print(list1[::-1])
+
+# # list1=[1,3,4,2]
+# # x=list1.pop(2)
+# # print(set([x]))
+
+# # str1='stack of books'
+# # print(len(str1))
+
+# # dict={'joey':1 ,'rachel':2}
+# # dict.update({'phon':2})
+# # print(dict)
+
+# # class test:
+# #     def __init__(self,a):
+# #         self.a=a
+
+# #     def display(self):
+# #         print(self.a)
+# # obj=test()
+# # obj.display()
+
+# # print(0.1+0.2==0.3)
+
+# # tupl=()
+# # tupl1=tupl*2
+# # print(len(tupl1))
+
+# # print('p'+'q' if '12'.isdigit() else 'r' +'s')
+
+# # set1={2,3}
+# # set2={3,2}
+# # set3={2,1}
+# # if(set1==set2):
+# #     print('yes')
+# # else:
+# #     print('no')
+# # if(set1==set3):
+# #     print('yes')
+# # else:
+# #     print('no')
+
+# # tuple1=(2,4,3)
+# # tuple3=tuple1*2
+# # print(tuple3)
+
+# # dict1={'a':10,'b':2,'c':3}
+# # str1=''
+# # for i in dict1:
+# #     str1=str1+str(dict1[i])+" "
+# #     str2=str1[:-1]
+# # print(str2[::-1])
+
+# # dict1={'a':1,'b':2,'c':3}
+# # dict1['a']=5
+
+# # set1={'a','b',3}
+# # set2={3,7}
+# # print(set1|set2)
+
+# # str='Aplication'
+# # str2=str.replace('a','A')
+# # print(str2)
+
+# # list1=[6,23,3,2,0,9,8,75]
+# # print(list1[1:7:2])
+# # print(list1[0:7:2])
+# # print(list1[1:8:2])
+# # print(list1[0:8:2])
+
+# # dict={'py':94 ,'c':70,'bio':82,'e':95}
+# # dict.update({'ch':72,'bio':80})
+# # print(dict)
+
+# str1='john,simon,aryan'
+# print(str1[-7:-12])
+# print(str1[-11:-7])
+# print(str1[-11:-6])
+# # print(str1[-7:-11])
+
+# tupl=([2,3],'abc',0,9)
+# tupl[0][1]=1
+# print(tupl)
+
+# def convl(s):
+# try:
